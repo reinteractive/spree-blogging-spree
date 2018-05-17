a47174a031b3ab0b1f54bb4bac825a0c575a04b1
99cm <zyizyao@china-guide.com>
2018-05-17 16:39:22 -0400

    Update hash syntax

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Spree::Core::Engine.routes.draw do
   end
 
   scope Spree::Config['blog_alias'], as: 'blog' do
-    get '/tag/:tag' => 'blog_entries#tag', :as => :tag
-    get '/category/:category' => 'blog_entries#category', :as => :category
-    get '/author/:author' => 'blog_entries#author', :as => :author
-    get '/:year/:month/:day/:slug' => 'blog_entries#show', :as => :entry_permalink
-    get '/:year(/:month)(/:day)' => 'blog_entries#archive', :as => :archive, 
-      :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
-    get '/feed' => 'blog_entries#feed', :as => :feed, :format => :rss
-    get '/' => 'blog_entries#index'
+    get '/tag/:tag', to: 'blog_entries#tag', as: :tag
+    get '/category/:category', to: 'blog_entries#category', as: :category
+    get '/author/:author', to: 'blog_entries#author', as: :author
+    get '/:year/:month/:day/:slug', to: 'blog_entries#show', as: :entry_permalink
+    get '/:year(/:month)(/:day)', to: 'blog_entries#archive', as: :archive, constraints: {year: /(19|20)\d{2}/, month: /[01]?\d/, day: /[0-3]?\d/}
+    get '/feed', to: 'blog_entries#feed', as: :feed, format: :rss
+    get '/', to: 'blog_entries#index'
   end
 end
-
