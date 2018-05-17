5c5869f734173c046ae48ac01bf67b94f2afd9e7
99cm <zyizyao@china-guide.com>
2018-05-17 14:04:47 -0400

    Update hash syntax

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,8 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "blog_admin_tabs",
-                     :insert_bottom => "#main-sidebar",
-                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab 'BLOG', url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
-                     :disabled => false)
-
-
-
+Deface::Override.new(virtual_path: "spree/layouts/admin",
+                     name: "blog_admin_tabs",
+                     insert_bottom: "#main-sidebar",
+                     text: "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab 'BLOG', url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
+                     disabled: false)
