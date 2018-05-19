c342dee69ff218b4255ecf46c0a9b5e77d991fcd
99cm <zyizyao@china-guide.com>
2018-05-18 20:45:45 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Spree::Core::Engine.routes.draw do
     get '/tag/:tag', to: 'blog_entries#tag', as: :tag
     get '/category/:category', to: 'blog_entries#category', as: :category
     get '/author/:author', to: 'blog_entries#author', as: :author
-    get '/:year/:month/:day/:slug', to: 'blog_entries#show', as: :entry_permalink
+    get '/:slug', to: 'blog_entries#show', as: :entry_permalink
     get '/:year(/:month)(/:day)', to: 'blog_entries#archive', as: :archive, constraints: {year: /(19|20)\d{2}/, month: /[01]?\d/, day: /[0-3]?\d/}
     get '/feed', to: 'blog_entries#feed', as: :feed, format: :rss
     get '/', to: 'blog_entries#index'
