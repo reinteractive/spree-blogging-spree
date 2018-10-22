789e91c9777b3971bda139b4d86c69d0657f5156
Charles Peach <me@charlespeach.com>
2018-10-23 10:59:28 +1300

    Add nice default paths for development and test for images

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,8 +1,10 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
-     styles: { mini: '150x100#', normal: '420x300>', large: '950x700>', cover: '1270x848#' },
-     default_style: :large,
-     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+    styles: { mini: '150x100#', normal: '420x300>', large: '950x700>', cover: '1270x848#' },
+    default_style: :large,
+    convert_options: { all: '-strip -auto-orient -colorspace sRGB' },
+    url: "/spree/blog_entry_image/:id/:style/:basename.:extension",
+    path: ":rails_root/public/spree/blog_entry_image/:id/:style/:basename.:extension"
 
   validates_attachment :attachment,
     presence: true,
