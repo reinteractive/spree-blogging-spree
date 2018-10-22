7c2a38518cd5f8b02c3857f7c8584badcf55c921
Charles Peach <me@charlespeach.com>
2018-10-23 10:00:05 +1300

    Remove url and path options from image in favor of default config

--- app/models/spree/blog_entry_cover.rb
+++ app/models/spree/blog_entry_cover.rb
@@ -2,9 +2,8 @@ class Spree::BlogEntryCover < Spree::Image
   has_attached_file :attachment,
      styles: { mini: '150x100#', normal: '420x300>', large: '950x700>', cover: '1270x848#' },
      default_style: :large,
-     convert_options: { all: '-strip -auto-orient -colorspace sRGB' },
-     url: "/assets/blog_entry_covers/:id/:style/:basename.:extension",
-     path: ":rails_root/public/assets/blog_entry_covers/:id/:style/:basename.:extension"
+     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+
   validates_attachment :attachment,
     presence: true,
     content_type: { content_type: %w(image/jpeg image/jpg image/png) }
